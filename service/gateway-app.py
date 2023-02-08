b5ebaf25cc3c42f8f4adab83af2d2cf69985d48e
Francesco Mergiotti <francescomergiotti.25@gmail.com>
2023-02-08 12:00:08 +0100

    Managed repo

--- service/gateway-app.py
+++ service/gateway-app.py
@@ -1,16 +1,15 @@
 from flask import Flask, jsonify, request, flash, request, redirect
 from flask_cors import CORS, cross_origin
+from PIL import Image
 from werkzeug.utils import secure_filename
 import os
-import carDetectionService
+import car_cetection_service as ccs
 
 app = Flask(__name__)
-
-model = carDetectionService.Model()
-
+model = ccs.Model()
 CORS(app, expose_headers='Authorization')
 
-@app.route("/upload-image", methods=['POST', 'GET'])
+@app.route("/test-image", methods=['POST', 'GET'])
 def uploadImage():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -20,24 +19,35 @@ def uploadImage():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        filename = secure_filename(file.filename)
+        img = Image.open(file)
+        img = img.convert('L')
+        img.save('./data/test/test.jpg')
+        [className, accuracy] = model.testModel("./data/test/test.jpg")
         response = {
-            'value': [1, 2, 3],
-            'message': "Image uploaded successfully: " + filename
+            'accuracy': accuracy,
+            'class': className
         }
         return jsonify(response)
     response = {
-        'value': [1, 2, 3],
-        'message': "Something went wrong!"
+        'message': "Something went wrong!",
+        'code': 500
     }
     return jsonify(response)
 
-@app.route("/car-dataset-info", methods=['GET'])
-def getCarDatasetInfo():
-    [className, accuracy] = model.testImage("./data/test/test.jpg")
+@app.route("/test-car-model", methods=['POST'])
+def testModel():
+    if request.method == 'POST':
+        print(request)
+        # [className, accuracy] = model.testModel("./data/test/test.jpg")
+        response = {
+            'accuracy': 10,
+            'class': 'className',
+            'message': 200
+        }
+        return jsonify(response)
     response = {
-        'accuracy': accuracy,
-        'class': className
+        'message': "Something went wrong!",
+        'code': 500
     }
     return jsonify(response)
 
@@ -45,4 +55,3 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.secret_key = os.urandom(24)
     app.run(debug=True, host="0.0.0.0", port=port, use_reloader=False)
-
