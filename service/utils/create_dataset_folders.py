6d98039ac6fd56dc5f253441ffd1e7cc33784401
Francesco Mergiotti <francescomergiotti.25@gmail.com>
2023-02-08 12:18:15 +0100

    Indexing images

--- service/utils/create_dataset_folders.py
+++ service/utils/create_dataset_folders.py
@@ -3,7 +3,7 @@ import re
 import os
 import shutil
 
-data_dir = pathlib.Path("data/cars")
+data_dir = pathlib.Path("../data/cars")
 
 for image in list(data_dir.glob('*.jpg')):
     x = re.search("\_20[0-9]{2}.*", str(image))
@@ -12,12 +12,12 @@ for image in list(data_dir.glob('*.jpg')):
     else:
         x = re.split("\_20[0-9]{2}.*", str(image))
     x = re.split("data\\\\cars\\\\", str(x[0]))
-    if os.path.exists("./data/cars/"+ str(x[1])):
-        new_directory = os.path.join("./data/cars/", x[1])
+    if os.path.exists("../data/cars/"+ str(x[1])):
+        new_directory = os.path.join("../data/cars/", x[1])
         x = re.split("data\\\\cars\\\\", str(image))
         shutil.move(image, new_directory + "/" + x[1])
     else:
-        new_directory = os.path.join("./data/cars/", x[1])
+        new_directory = os.path.join("../data/cars/", x[1])
         os.mkdir(new_directory)
         x = re.split("data\\\\cars\\\\", str(image))
         shutil.move(image, new_directory + "/" + x[1])
